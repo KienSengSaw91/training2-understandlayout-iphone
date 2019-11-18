50fc199f991af80e2698537666e160cf27cbad4a
Gordan Saw <ks.saw@appdevteam.work>
2019-11-18 13:20:34 +0800

    Initial Commit

---
+++ Training2 Understand Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training2 Understand Layout
+//
+//  Created by Gordan Saw on 18/11/2019.
+//  Copyright © 2019 Gordan Saw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
